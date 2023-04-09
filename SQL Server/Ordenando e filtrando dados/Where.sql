c45274f6ed2e8cca49d093ba4417357e1ca7a3a1
LucasPPontes <lucaspontes360@gmail.com>
2023-04-09 18:31:29 -0300

    'done'

--- SQL Server/Ordenando e filtrando dados/Where.sql
+++ SQL Server/Ordenando e filtrando dados/Where.sql
@@ -4,14 +4,25 @@ USE ContosoRetailDW;
     SELECT UnitPrice FROM DimProduct
     ORDER BY UnitPrice DESC 
 
-    -- Quantos produtos tem o preço unitário maior que $1000?
+    -- Filtrando uma coluna de números com WHERE
     SELECT ProductName, UnitPrice 
         FROM DimProduct
         WHERE UnitPrice >= 1000;
 
+    -- Filtrando uma coluna de letras com WHERE
 
-    --SELECT * FROM DimProduct;
+    SELECT * FROM DimProduct;
     SELECT DISTINCT BrandName FROM DimProduct
 
     SELECT * FROM DimProduct
         WHERE BrandName = 'Fabrikam'
+
+    SELECT * FROM DimProduct
+        WHERE ColorName = 'Black'
+
+    -- Filtrando uma coluna de datas com WHERE
+    -- Quantos clientes nasceram após o dia 31/12/1970
+
+    SELECT * FROM DimCustomer
+        WHERE BirthDate >= '1970-12-31'
+        ORDER BY BirthDate DESC
