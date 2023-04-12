0faa8e917710e8bd1a7e59debb55585038d66143
LucasPPontes <lucaspontes360@gmail.com>
2023-04-12 13:51:53 -0300

    'done'

--- SQL Server/Criando agrupamentos/Exercicios.sql
+++ SQL Server/Criando agrupamentos/Exercicios.sql
@@ -1,12 +1,10 @@
 -- FACTSALES
--- 1 a) faça um resumo da quantidade vendida (salesquantity) de acordo com o canal de vendas (channelkey)
--- b) Faça um agrupamento mostrando a quantidade total vendida (salesquantity) e quantidade total devolvida (return quantity) de acordo com o ID das lojas (storekey)
--- c) faça um resumo do valor total vendido (salesamount) para cada canal de venda, mas apenas para o ano de 2007
-
+-- 1 
+-- a) faça um resumo da quantidade vendida (salesquantity) de acordo com o canal de vendas (channelkey)
 USE ContosoRetailDW;
 
     SELECT * FROM FACTSALES
--- a)
+
     SELECT 
         ChannelKey,
         SUM(SalesQuantity) AS 'Quantidade vendida'
@@ -14,15 +12,16 @@ USE ContosoRetailDW;
         FactSales
     GROUP BY channelKey
 
--- b)
+-- b) Faça um agrupamento mostrando a quantidade total vendida (salesquantity) e quantidade total devolvida (return quantity) de acordo com o ID das lojas (storekey)
+
     SELECT  
         StoreKey,
         SUM(SalesQuantity) AS 'Quantidade total vendida',
         SUM(ReturnQuantity) AS 'Quantidade total devolvida'
     FROM FactSales
     GROUP BY StoreKey
-
--- c) 
+-- c) faça um resumo do valor total vendido (salesamount) para cada canal de venda, mas apenas para o ano de 2007
+ 
     SELECT 
         ChannelKey,
         SUM(SalesAmount)
@@ -32,11 +31,10 @@ USE ContosoRetailDW;
     
 -- 2 Você precisa fazer uma análise de vendas por produtos. O objetivo final é descobrir o valor total vendido (SalesAmount) por produto
 -- a) A tabela final deverá estar ordenada de acordo com a quantidade vendida e, além disso, mostrar apenas os produtos que tiveram um resultado final de vendas maior que $5.000.000
--- b) Faça uma adaptação no exercício anterior e mostra os Top 10 produtos com mais vendas. Desconsidere o filtro de $5.000.000
-
+   
 USE ContosoRetailDW;
     SELECT TOP(10) * FROM FactSales
--- a)    
+
     SELECT
         ProductKey AS 'Chave Produto',
         SUM(SalesAmount) AS 'Valor total vendido'
@@ -44,17 +42,24 @@ USE ContosoRetailDW;
         FactSales
     GROUP BY ProductKey
     HAVING SUM(SalesAmount) > 5000000
--- b)
+    ORDER BY SUM(SalesAmount) DESC
+-- b) Faça uma adaptação no exercício anterior e mostra os Top 10 produtos com mais vendas. Desconsidere o filtro de $5.000.000
+
+    SELECT TOP(10)
+        ProductKey AS 'Chave Produto',
+        SUM(SalesAmount) AS 'Valor total vendido'
+    FROM 
+        FactSales
+    GROUP BY ProductKey
     ORDER BY SUM(SalesAmount) DESC
 
 -- FACTONLINESALES
--- 3. a) Você deve fazer uma consulta à tabela FactOnlineSales e descobrir qual é o ID (CustomerKey) do cliente que mais realizou compras online (de acordo com a coluna SalesQuantity)
--- b) Feito isso, faça um agrupamento de total vendido (SalesQuantity) por ID do produto e descubra quais foram os top 3 produtos mais comprados pelo cliente da letra a)
-
+-- 3. 
+-- a) Você deve fazer uma consulta à tabela FactOnlineSales e descobrir qual é o ID (CustomerKey) do cliente que mais realizou compras online (de acordo com a coluna SalesQuantity)
 USE ContosoRetailDW;
     
     SELECT TOP(1000)* FROM FactOnlineSales
--- a)
+
     SELECT TOP(1)
         CustomerKey AS 'Chave cliente',
         SUM(SalesQuantity) AS 'Total de compras'
@@ -62,8 +67,8 @@ USE ContosoRetailDW;
         FactOnlineSales
     GROUP BY CustomerKey
     ORDER BY SUM(SalesQuantity) DESC  
+-- b) Feito isso, faça um agrupamento de total vendido (SalesQuantity) por ID do produto e descubra quais foram os top 3 produtos mais comprados pelo cliente da letra a)
 
--- b)
     SELECT TOP(3)
         ProductKey AS 'Chave produto',
         SUM(SalesQuantity) AS 'Total vendido'
@@ -72,26 +77,26 @@ USE ContosoRetailDW;
     ORDER BY SUM(SalesQuantity) DESC
 
 -- DIMPRODUCT
--- 4. a)Faça um agrupamento e descubra a quantidade total de produtos por marca
--- b) determine a média do preço unitário (unitprice) para cada ClassName
--- c) Faça um agrupamento de cores e descubra o peso total que cada cor de produto possui
+-- 4. 
+-- a)Faça um agrupamento e descubra a quantidade total de produtos por marca
 USE ContosoRetailDW;
 
     SELECT * FROM DimProduct
--- a)
+
     SELECT
         BrandName AS 'Marca',
         COUNT(ProductName) AS 'Qtde Produtos'
     FROM DimProduct
     GROUP BY BrandName
--- b)
+-- b) determine a média do preço unitário (unitprice) para cada ClassName
+
     SELECT 
         ClassName,
         AVG(UnitPrice) AS 'Media preço'
     FROM
         DimProduct
     GROUP BY ClassName
--- c)
+-- c) Faça um agrupamento de cores e descubra o peso total que cada cor de produto possui
     SELECT
         ColorName,
         SUM(Weight) AS 'Peso total'
